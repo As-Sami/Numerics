61e6d66676de7bfa2befaeb394be5e2913530a50
As-Sami <rssami.bd@gmail.com>
2022-10-14 18:36:07 +0600

    initial commit

---
+++ Linear_Equation_Solver.py
@@ -0,0 +1,213 @@
+a = [
+    [25, 5, 1],
+    [64, 8, 1],
+    [144, 12, 1]
+]
+
+b = [
+    106.8,
+    177.2,
+    279.2
+]
+
+def print_matrix(a, d=3):
+    for i in a:
+        for j in i:
+            print(format(j, f'.{d}f'), end=' ')
+        print('')
+    print('')
+
+
+def NGE(A: list, b: list):
+    '''Naive Gaussian Elemination'''
+    n = len(A)   
+    
+    a = []
+    
+    for x in A:
+        e = [];
+        for y in x:
+            e.append(y)
+        a.append(e)
+    
+    for i in range(n):
+        a[i].append(b[i])
+        
+    for k in range(n-1):
+        
+        temp = a[k].copy()
+        val =  temp[k]
+        for i in range(len(temp)):
+            try:
+                temp[i] /= val
+            except:
+                print('divison by zero')
+                print("Naive Gaussian Elemination failed")
+                return
+        
+        for i in range(k+1, n):
+            temp2 = temp.copy()
+            for j in range(len(temp2)):
+                temp2[j] *= a[i][k]
+            
+            for j in range(len(a[i])):
+                a[i][j] -= temp2[j]
+    
+    solution = [0] * n
+    for i in range(n-1, -1, -1):
+        j = i+1
+        while j<n:
+            a[i][n] -= a[i][j]*solution[j]
+            j += 1
+        
+        solution[i] = a[i][n] / a[i][i]
+        
+    return solution
+    
+def GEPP(A: list, b: list):
+    '''Gaussian Elemination with Partial Pivoting'''
+    n = len(A)   
+    
+    a = []
+    
+    for x in A:
+        e = [];
+        for y in x:
+            e.append(y)
+        a.append(e)
+    
+    for i in range(n):
+        a[i].append(b[i])
+        
+    for k in range(n-1):
+        pos = k
+        val = a[k][k]
+        
+        for i in range(k+1, n):
+            if val < a[i][k]:
+                pos = i
+                val = a[i][k]
+        
+        a[k], a[pos] = a[pos], a[k]
+              
+        temp = a[k].copy()
+        val =  temp[k]
+        for i in range(len(temp)):
+            try:
+                temp[i] /= val
+            except:
+                print('divison by zero')
+                return
+        
+        for i in range(k+1, n):
+            temp2 = temp.copy()
+            for j in range(len(temp2)):
+                temp2[j] *= a[i][k]
+            
+            for j in range(len(a[i])):
+                a[i][j] -= temp2[j]
+    
+        
+    solution = [0] * n
+    for i in range(n-1, -1, -1):
+        j = i+1
+        while j<n:
+            a[i][n] -= a[i][j]*solution[j]
+            j += 1
+        
+        solution[i] = a[i][n] / a[i][i]
+        
+    return solution
+
+def LU(a: list):
+    '''Converts a matrix to a upper and lower triangula matrix, A = L * U'''
+    n = len(a)
+    
+    L = [[0 for i in range(n)] for j in range(n)]
+    
+    U = []
+    
+    for x in a:
+        e = [];
+        for y in x:
+            e.append(y)
+        U.append(e)
+    
+    for i in range(3):
+        L[i][i] = 1      
+    
+    for k in range(n-1):
+        pos = k
+        val = U[k][k]
+        
+        temp = [1 for i in range(n)]
+        for i in range(len(temp)):
+            try:
+                temp[i] /= val
+            except:
+                print('divison by zero')
+                print("LU decomposition failed")
+                return None
+        
+        for i in range(k+1, n):
+            temp2 = temp.copy()
+            
+            for j in range(len(temp2)):
+                temp2[j] *= U[i][k]
+            
+            L[i][k] = temp2[i]
+            
+            for j in range(len(a[i])):
+                U[i][j] -= U[k][j]*temp2[j]
+           
+    return L, U
+
+def LU_Decomposition(a, b):
+    '''LU Decomposition'''
+    n = len(a)
+    L, U = LU(a)
+
+    Z = GEPP(L, b)
+    
+    for i in range(len(Z)):
+        U[i].append(Z[i])
+    
+    solution = [0] * n
+    for i in range(n-1, -1, -1):
+        j = i+1
+        while j<n:
+            U[i][n] -= U[i][j]*solution[j]
+            j += 1
+        
+        solution[i] = U[i][n] / U[i][i]
+    
+    return solution
+
+def Inverse(a):
+    '''Find the inverse of a matrix using LU decomposition method'''
+    
+    n = len(a)
+    solution = []
+    for i in range(n):
+        solution.append([])
+        
+    for i in range(n):
+        C = [0] * n
+        C[i] = 1
+        
+        x = LU_Decomposition(a, C)
+        print(x)
+        
+        for j in range(n):
+            solution[j].append(x[j])
+  
+    return solution
+    
+
+# print_matrix(L)
+# print_matrix(U)
+
+# print(GEPP(a, b))
+# print(NGE(a, b))
+# print(LU_Decomposition(a,b))
+# print_matrix(Inverse(a))
---
+++ Root_Finder.py
@@ -0,0 +1,184 @@
+from sympy import diff, symbols
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+import numpy as np
+
+def Draw_Graph(exp, start, end):
+    
+    def f(x):
+        return eval(exp.replace('x', str(x)))
+    
+    xx = []
+    yy = []
+    
+    while start < end:
+        xx.append(start)
+        yy.append(f(start))
+        start += 0.01
+        
+    plt.axhline(y=0.0, color='r', linestyle='-')
+    plt.axvline(x=0.0, color='r')
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    
+    plt.plot(np.array(xx), np.array(yy))
+    plt.show()
+    
+
+def Bisection(exp, tolerance, iteration, first_guess, second_guess):
+    
+    def f(x):
+        return eval(exp.replace('x', str(x)))
+    
+    u = first_guess
+    l = second_guess
+
+    if f(u) * f(l) > 0:
+        print('The initial guesses are the same sign... Please guess again')
+        return None, None
+
+    prev = None
+    root = None
+    table = [('Iteration', 'x(l)', 'x(u)', 'x(m)', 'error', 'f(x(m))')]
+
+    for it in range(1, iteration):
+        
+        m = (u+l) / 2
+        val_u = round(f(u), 6)
+        val_m = round(f(m), 6)
+        val_l = round(f(l), 6)
+        
+        err = None if prev is None else abs(abs(prev - m)/m) * 100
+
+        table.append((it+1, l, u, m, err, val_m))
+        prev = m
+        
+        if err and err < tolerance:
+            root = m
+            break
+        
+        if val_l * val_m < 0:
+            u = m
+        elif val_l * val_m > 0:
+            l = m
+        else:
+            root = m
+            break
+        
+    return root, table
+    
+def FalsePosition(exp, tolerance, iteration, first_guess, second_guess):
+    
+    def f(x):
+        return eval(exp.replace('x', str(x)))
+    
+    u = first_guess
+    l = second_guess
+    
+    if f(u) * f(l) > 0:
+        print('The initial guesses are the same sign... Please guess again')
+        return None, None
+    
+    prev = None
+    root = None
+    table = [('Iteration', 'x(l)', 'x(u)', 'x(m)', 'error', 'f(x(m))')]
+
+    for it in range(1, iteration):
+        
+        val_u = f(u)
+        val_l = f(l)
+        
+        m = (u * val_l - l * val_u) / (val_l - val_u)
+        val_m = f(m)
+        
+        err = None if prev is None else abs(abs(prev - m)/m) * 100
+                    
+        table.append((it+1, l, u, m, err, val_m))
+        
+        if err and err < tolerance:
+            root = m
+            break
+        
+        if val_l * val_m < 0:
+            u = m
+        elif val_l * val_m > 0:
+            l = m
+        else:
+            root = m
+            break
+        
+        prev = m
+        
+    return root, table
+
+def NewtonRaphson(exp, tolerance, iteration, initial_guess):
+
+    def f(x):
+        return eval(exp.replace('x', str(x)))
+    
+    d_exp = str(diff(exp, symbols('x')))
+    
+    def df(x):
+        return eval(d_exp.replace('x', str(x)))
+
+    x = initial_guess
+    root = None
+    table = [('Iteration', 'x(i)', 'x(i+1)', 'error')]
+    
+    for it in range(1, iteration):
+        y = x - f(x)/df(x)
+        
+        err = abs((y - x)/y) * 100
+        
+        table.append((it, x, y, err))
+        x = y
+        it += 1
+        
+        if err > tolerance:
+            continue
+        else:
+            root = x
+            break
+    
+    return root, table
+    
+def Secant(exp, tolerance, itetarion, first_guess, second_guess):
+
+    def f(x):
+        return eval(exp.replace('x', str(x)))
+    
+    x = first_guess
+    y = second_guess
+    table = [('Iteration', 'x(i-1)', 'x(i)', 'x(i+1)', 'error')]
+    root = None
+    
+    for it in range(1, itetarion+1):
+        z = y - (f(y) * (y-x)) / (f(y) - f(x))
+        
+        err = abs((z - y)/z) * 100
+        
+        table.append((it, x, y, z, err))
+        x = y
+        y = z
+        
+        if err > tolerance:
+            continue
+        else:
+            root = x
+            break
+    
+    return root, table
+    
+def Show_A(info):
+    root, table = info
+    print('Root =', root)
+    print(tabulate(table[1:], headers=table[0]))
+    
+expression = 'x**3 - 0.165*x**2 + 3.993e-4'
+Show_A(Bisection(expression, 0.5, 10, 0, 0.11))
+Show_A(FalsePosition(expression, 0.05, 10, 0, 0.11))
+
+Show_A(NewtonRaphson(expression, 2, 10, 0.05))
+Show_A(Secant(expression, 0.5, 5, 0.02, 0.05))
+
+# Draw_Graph(expression, 0, 0.11)
