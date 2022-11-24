65dd7208005bb41522716d475194834837ca6e96
As-Sami <rssami.bd@gmail.com>
2022-11-24 21:11:33 +0600

    simpson

--- Assignment-2/Numerical_Integration.py
+++ Assignment-2/Numerical_Integration.py
@@ -11,14 +11,42 @@ def Trapezoidal(exp, a, b, n):
         result += 2 * f(a + i*h)
         
     result *= (b-a) / (2*n)
-    print(result)
+    return result
+    
+def Simpson(exp, a, b, n):
+    def f(x):
+        from math import log, sin, cos, tan, e, pi
+        return eval(exp.replace('x', str(x)))
+    
+    h = (b - a) / n
+    
+    result = f(a) + f(b)
+    for i in range(1, n, 2):
+        result += 4 * f(a + i*h)
+    
+    for i in range(2, n-1, 2):
+        result += 2 * f(a + i*h)
+        
+    result *= (b-a) / (3 * n)
+    return result
 
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 1)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 2)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 3)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 4)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 5)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 6)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 7)
-# Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 8)
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 1))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 2))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 3))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 4))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 5))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 6))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 7))
+# print(Trapezoidal('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 8))
+
+# print('')
+
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 1))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 2))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 3))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 4))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 5))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 6))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 7))
+# print(Simpson('2000 * log(140000/ (140000 - 2100*x)) - 9.8 * x', 8, 30, 8))
 
